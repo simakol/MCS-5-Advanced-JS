994c5be1951c9292b2bc2b332bf1b81e1cd8a82f
Alexey Simak <>
2024-10-08 18:59:04 +0300

    lesson-05

---
+++ src/js/02-gallery.js
@@ -0,0 +1,11 @@
+// https://unsplash.com/documentation
+// N2hFKxqELoV2Hd6dcIRJn1oRrjfp310WTtPLEXfMXjg
+
+/*
+<li class="gallery-card">
+  <img class="gallery-img" src="" alt="" />
+</li>
+*/
+
+const searchFormEl = document.querySelector('.js-search-form');
+const galleryEl = document.querySelector('.js-gallery');
